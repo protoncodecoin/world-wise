05018873401b76595d47552bd87e60fbdd09be16
Prince Affum <princeaffumasante@gmail.com>
2025-10-14 11:09:43 +0000

    initial commit

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,21 @@
+import AppNav from "./AppNav";
+import Logo from "./Logo";
+import styles from "./Sidebar.module.css";
+
+function Sidebar() {
+  return (
+    <div className={styles.sidebar}>
+      <Logo />
+      <AppNav />
+
+      <p>Lis of cities</p>
+      <footer className={styles.footer}>
+        <p className={styles.copyright}>
+          &copy; Copyright {new Date().getFullYear()} by Prince Corp.
+        </p>
+      </footer>
+    </div>
+  );
+}
+
+export default Sidebar;
